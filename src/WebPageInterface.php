80e93fd5722c8b984f0f2a48ca29ebd76acdf5e9
Jon Cram <jon@webignition.net>
2018-10-02 15:40:07 +0100

    Remove WebPageInterface::getDocumentCharacterSet()

--- src/WebPageInterface.php
+++ src/WebPageInterface.php
@@ -8,18 +8,9 @@ namespace webignition\WebResourceInterfaces;
 interface WebPageInterface extends WebResourceInterface
 {
     /**
-     * Returns, in order of preference, first found to be valid of:
-     *  - document character set
-     *  - http response character set
-     *
-     * @return string|null
-     */
-    public function getCharacterSet(): ?string;
-
-    /**
      * Returns the character set found in the document i.e. the content of the WebResourceInterface instance.
      *
      * @return string
      */
-    public function getDocumentCharacterSet(): string;
+    public function getCharacterSet(): string;
 }

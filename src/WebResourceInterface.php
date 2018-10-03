1011150ba618c26a4a213b74dd58a7a5d1608e13
Jon Cram <jon@webignition.net>
2018-10-03 09:11:45 +0100

    Add ResponseResourceInterface methods to WebResourceInterface

--- src/WebResourceInterface.php
+++ src/WebResourceInterface.php
@@ -2,6 +2,8 @@
 
 namespace webignition\WebResourceInterfaces;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UriInterface;
 use webignition\InternetMediaTypeInterface\InternetMediaTypeInterface;
 
@@ -63,6 +65,48 @@ interface WebResourceInterface
      * @return string
      */
     public function getContent(): ?string;
+
+    /**
+     * Return an instance with the specified response.
+     *
+     * The response MUST be a ResponseInterface object.
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the resource,
+     * and MUST return a new instance that has the new response.
+     *
+     * @param ResponseInterface $response
+     *
+     * @return ResponseResourceInterface
+     */
+    public function setResponse(ResponseInterface $response): ResponseResourceInterface;
+
+    /**
+     * Gets the response of the resource.
+     *
+     * @return ResponseInterface
+     */
+    public function getResponse(): ?ResponseInterface;
+
+    /**
+     * Return an instance with the specified body.
+     *
+     * The body MUST be a StreamInterface object.
+     *
+     * @param StreamInterface $body
+     *
+     * This method MUST be implemented in such a way as to retain the immutability of the resource,
+     * and MUST return a new instance that has the new body.
+     *
+     * @return ResponseResourceInterface
+     */
+    public function setBody(StreamInterface $body): ResponseResourceInterface;
+
+    /**
+     * Gets the body of the resource.
+     *
+     * @return StreamInterface|null
+     */
+    public function getBody(): ?StreamInterface;
 
     /**
      * Does this instance provide a model for a given internet media type?
